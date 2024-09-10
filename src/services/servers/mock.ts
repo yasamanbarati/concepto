9fc73b6af601b6c55a69b9937a14fd3e8c6fd742
yasamanbarati <yasamanbarati81@gmail.com>
2024-09-11 00:14:54 +0330

    feat: Improvement file structure(images,mock data)

--- src/services/servers/mock.ts
+++ src/services/servers/mock.ts
@@ -32,3 +32,75 @@ export const MapRoad: string[] = [
   "کسب و کارهای مناسب رو پیدا کن",
   "با کسب و کارها ارتباط بگیر",
 ];
+export const Categories =[
+  {
+    id: 0,
+    image:'/images/Button.png',
+    name: 'هوش مصنوعی',
+  },
+  {
+    id: 1,
+    image:'/images/Button (1).png',
+    name: 'برنامه نویسی',
+  },
+  {
+    id: 2,
+    image:'/images/Button (2).png',
+    name: 'ارز دیجیتال',
+  },
+  {
+    id: 3,
+    image:'/images/Switch.png',
+    name: 'حسابداری',
+  },
+  {
+    id: 4,
+    image:'/images/Button (3).png',
+    name: 'گرافیک',
+  },
+  {
+    id: 5,
+    image:'/images/Switch (1).png',
+    name: 'رسانه‌ها',
+  },
+  {
+    id: 6,
+    image:'/images/Switch (2).png',
+    name: 'هوافضا',
+  },
+  {
+    id: 7,
+    image:'/images/Switch (3).png',
+    name: 'پزشکی',
+  },
+  {
+    id: 8,
+    image:'/images/Switch (4).png',
+    name: 'تجارت الکترونیک',
+  },
+  {
+    id: 9,
+    image:'/images/Vertical container.png',
+    name: 'علمی',
+  },
+  {
+    id: 10,
+    image:'/images/Switch (5).png',
+    name: 'موسیقی',
+  },
+  {
+    id: 11,
+    image:'/images/Switch (6).png',
+    name: 'بیوتکنولوژی',
+  },
+  {
+    id: 12,
+    image:'/images/Button (4).png',
+    name: 'فیلم و سینما',
+  },
+  {
+    id: 13,
+    image:'/images/Slider.png',
+    name: 'حمل و نقل',
+  },
+]
