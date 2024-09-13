0029d547d57f5774e345303e15cde6c3a9da9818
yasamanbarati <yasamanbarati81@gmail.com>
2024-09-13 13:22:09 +0330

    feat: development code structure(2:category section)

--- src/components/UI/home/category-section/index.tsx
+++ src/components/UI/home/category-section/index.tsx
@@ -5,9 +5,15 @@ import { Categories } from "@/services/servers/mock";
 import { Grid2, styled } from "@mui/material";
 import Image from "next/image";
 
+const CategoryImage = styled(Image)(({ theme }) => ({
+  border: `2px solid ${theme.palette.primary.main}`,
+  borderRadius: "50%",
+  padding: "4px",
+}));
+
 const CategorySection = () => {
   return (
-    <TitleSection title="دسته بندی ها">
+    <TitleSection title="دسته بندی ها" flag={false} textColor="gray">
       <Grid2 container spacing={12}>
         {Categories.map((item, index) => {
           return (
@@ -20,7 +26,12 @@ const CategorySection = () => {
               alignItems="center"
               gap="16px"
             >
-              <Image src={item.image} alt={item.name} width={84} height={84} />
+              <CategoryImage
+                src={item.image}
+                alt={item.name}
+                width={84}
+                height={84}
+              />
               <p>{item.name}</p>
             </Grid2>
           );
