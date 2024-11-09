863bc5e45565925e0f21b859de5b1f802a8580db
yasamanbarati <yasamanbarati81@gmail.com>
2024-11-09 18:07:14 +0330

    feat: Improvement services structure(slice,store)

--- src/_slice/type.ts
+++ src/_slice/type.ts
@@ -1,8 +1,16 @@
-import { CompaniesProps, LatestNewsProps } from "@/services/servers/type";
+import {
+  CompaniesProps,
+  ImportantNewsProps,
+  LatestNewsProps,
+} from "@/services/servers/type";
 
 export interface ReduxBodyType {
   allCompanies: CompaniesProps[];
   popularCompanies: CompaniesProps[];
   jobOpportunity: CompaniesProps[];
   latestNews: LatestNewsProps[];
+}
+export interface NewsEventBodyType {
+  ImportantNews: ImportantNewsProps[];
+  News: ImportantNewsProps[];
 }
--- src/setup/store/index.ts
+++ src/setup/store/index.ts
@@ -1,11 +1,13 @@
 import homeSlice from "@/_slice/home.slice";
+import newsEventsSlice from "@/_slice/news&events.slice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
-    home: homeSlice
+    home: homeSlice,
+    NewsEvents: newsEventsSlice,
   },
 });
-export type RootState = ReturnType<typeof store.getState>;  
-export type AppDispatch = typeof store.dispatch; 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export const { dispatch, getState } = store;
