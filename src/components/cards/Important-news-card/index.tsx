237fc5b32a4bf5efaf4087eab8d5724ca2de89b0
yasamanbarati <yasamanbarati81@gmail.com>
2024-11-09 18:11:48 +0330

    feat:development code structure(news card)

---
+++ src/components/cards/Important-news-card/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Image from "next/image";
+import { Grid2, styled } from "@mui/material";
+
+import { ImportantNewsProps } from "@/services/servers/type";
+
+const CardBox = styled(Grid2)(({ theme }) => ({
+  position: "relative",
+  "& img": {
+    position: "relative!important",
+    borderRadius: "16px",
+  },
+  "& p": {
+    position: "absolute",
+    padding: "16px",
+    color: theme.palette.white,
+    boxShadow: `inset 0 0 100px 0px ${theme.palette.neutral.dark}`,
+    borderRadius: " 0 0 16px 16px",
+  },
+}));
+
+const ImportantNewsCard = ({
+  image,
+  description,
+  size,
+}: ImportantNewsProps) => {
+  return (
+    <CardBox size={size}>
+      <Image alt="news" src={image} fill />
+      <p>{description}</p>
+    </CardBox>
+  );
+};
+
+export default ImportantNewsCard;
